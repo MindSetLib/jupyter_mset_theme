b03c74bdb4a371ccc534e05fbbe79fb857fd6a2b
alexmindset <alex@mind-set.ru>
2023-07-21 04:14:24 +0300

    Override logo with css and png

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,7 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { IThemeManager } from '@jupyterlab/apputils';
-import { LabIcon } from '@jupyterlab/ui-components';
-import msetSvgstr from './../style/images/mset.svg';
-
+import "../style/index.css"
 
 /**
  * Initialization data for the jupyter_mset_theme extension.
@@ -14,34 +11,8 @@ import msetSvgstr from './../style/images/mset.svg';
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyter_mset_theme:plugin',
   autoStart: true,
-  requires: [IThemeManager],
-  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+  activate: (app: JupyterFrontEnd) => {
     console.log('JupyterLab extension jupyter_mset_theme is activated!');
-    const style = 'jupyter_mset_theme/index.css';
-
-    const msetIcon = new LabIcon({name: 'ui-components:mset', svgstr: msetSvgstr});
-    const widgets = app.shell.widgets('top');
-    let widget = widgets.next();
-    while (widget !== undefined) {
-      if (widget.id === 'jp-MainLogo') {
-        msetIcon.element({
-          container: widget.node,
-          justify: 'center',
-          margin: '2px 5px 2px 5px',
-          height: 'auto',
-          width: '20px'
-        });
-        break;
-      }
-      widget = widgets.next();
-    }
-
-    manager.register({
-      name: 'jupyter_mset_theme',
-      isLight: true,
-      load: () => manager.loadCSS(style),
-      unload: () => Promise.resolve(undefined)
-    });
   }
 };
 
