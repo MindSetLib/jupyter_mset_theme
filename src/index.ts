16ba126c5d0e0069b445e53040d104eab7a1a6e0
alexmindset <alex@mind-set.ru>
2023-07-22 17:31:32 +0300

    Change logos and favicons in jupyterlab

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 import { Widget } from '@lumino/widgets';
-import { LabIcon } from '@jupyterlab/ui-components';
+import { LabIcon, jupyterFaviconIcon } from '@jupyterlab/ui-components';
 import msetSvgstr from './../style/images/mset.svg';
 import "../style/base.css"
 
@@ -15,6 +15,8 @@ const plugin: JupyterFrontEndPlugin<void> = {
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
     console.log('JupyterLab extension jupyter_mset_theme is activated!');
+
+    // Logo in the top-left corner
     const msetIcon = new LabIcon({name: 'ui-components:mset', svgstr: msetSvgstr});
     const logo = new Widget();
     msetIcon.element({
@@ -27,6 +29,14 @@ const plugin: JupyterFrontEndPlugin<void> = {
     });
     logo.id = 'jp-MsetLogo';
     app.shell.add(logo, 'top', { rank: 0 });
+
+    // Logo loading jupyter
+    jupyterFaviconIcon.svgstr = msetSvgstr;
+
+    // Favicon logo
+    const favicon = 'https://raw.githubusercontent.com/MindSetLib/jupyter_mset_theme/master/style/images/favicon.ico'
+    let faviconLink = document.getElementsByClassName('idle favicon')[0];
+    faviconLink.setAttribute('href', favicon);
   }
 };
 
