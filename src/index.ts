d7ab1f721795b99278ef0665e148935f12723a92
alexmindset <alex@mind-set.ru>
2026-04-02 17:44:25 +0300

    Update theme for JupyterLab 4

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import {
 import { Widget } from '@lumino/widgets';
 import { LabIcon, jupyterFaviconIcon } from '@jupyterlab/ui-components';
 import msetSvgstr from './../style/images/mset.svg';
-import "../style/base.css"
+import '../style/base.css';
 
 /**
  * Initialization data for the jupyter_mset_theme extension.
@@ -14,18 +14,19 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyter_mset_theme:plugin',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyter_mset_theme is activated!');
-
     // Logo in the top-left corner
-    const msetIcon = new LabIcon({name: 'ui-components:mset', svgstr: msetSvgstr});
+    const msetIcon = new LabIcon({
+      name: 'ui-components:mset',
+      svgstr: msetSvgstr
+    });
     const logo = new Widget();
     msetIcon.element({
-        container: logo.node,
-        display: 'block',
-        elementPosition: 'center',
-        margin: '2px 5px 2px 5px',
-        height: 'auto',
-        width: '20px'
+      container: logo.node,
+      display: 'block',
+      elementPosition: 'center',
+      margin: '2px 5px 2px 5px',
+      height: 'auto',
+      width: '20px'
     });
     logo.id = 'jp-MsetLogo';
     app.shell.add(logo, 'top', { rank: 0 });
@@ -34,9 +35,17 @@ const plugin: JupyterFrontEndPlugin<void> = {
     jupyterFaviconIcon.svgstr = msetSvgstr;
 
     // Favicon logo
-    const favicon = 'https://raw.githubusercontent.com/MindSetLib/jupyter_mset_theme/master/style/images/favicon.ico'
-    let faviconLink = document.getElementsByClassName('idle favicon')[0];
-    faviconLink.setAttribute('href', favicon);
+    const faviconLink =
+      document.querySelector<HTMLLinkElement>("link[rel~='icon']") ??
+      document.createElement('link');
+
+    faviconLink.rel = 'icon';
+    faviconLink.type = 'image/svg+xml';
+    faviconLink.href = `data:image/svg+xml,${encodeURIComponent(msetSvgstr)}`;
+
+    if (!faviconLink.parentElement) {
+      document.head.appendChild(faviconLink);
+    }
   }
 };
 
